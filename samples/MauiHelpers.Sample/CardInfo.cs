6c5da66e308a942232d3fe1e7d3877e6964f9b82
Stephen Quan <squan@esri.com>
2025-06-02 21:16:03 +1000

    Convert Count sample to CardView sample

---
+++ samples/MauiHelpers.Sample/CardInfo.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+namespace MauiHelpers.Sample;
+
+public partial class CardInfo : ObservableObject
+{
+	[ObservableProperty] public partial string CardTitle { get; set; } = "Card Title";
+	[ObservableProperty] public partial string CardDescription { get; set; } = "This is a sample card description. It can be used to provide additional information about the card.";
+	[ObservableProperty] public partial string CardTemplate { get; set; } = "CardViewCompressed";
+}
--- samples/MauiHelpers.Sample/MainPage.xaml.cs
+++ samples/MauiHelpers.Sample/MainPage.xaml.cs
@@ -1,22 +1,51 @@
-﻿using MauiHelpers.Mvvm;
+﻿using System.Collections.ObjectModel;
+using MauiHelpers.Mvvm;
 
 namespace MauiHelpers.Sample;
 
 public partial class MainPage : ContentPage
 {
-	[BindableProperty]
-	public partial int Count { get; set; } = 0;
+	public ObservableCollection<CardInfo> Cards { get; } =
+	[
+		new CardInfo
+		{
+			CardTitle = "Slavko Vlasic",
+			CardDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elit dolor, convallis non interdum.",
+		},
+		new CardInfo
+		{
+			CardTitle = "Carolina Pena",
+			CardDescription = "Phasellus eu convallis mi. In tempus augue eu dignissim fermentum. Morbi ut lacus vitae eros lacinia.",
+		},
+		new CardInfo
+		{
+			CardTitle = "Wade Banks",
+			CardDescription = "Aliquam sagittis, odio lacinia fermentum cictum, mi erat sccelerisque erat; quis alliquet arou.",
+		},
+		new CardInfo
+		{
+			CardTitle = "Colette Quint",
+			CardDescription = "In pellenteqsuq odio ecet augue elemntum lobortis. Sed augue masso, rhoocus eu nisi vitae, egestas.",
+		}
+	];
+
+	[ObservableProperty] public partial CardInfo? SelectedCard { get; set; } = null;
+
+	partial void OnSelectedCardChanged(CardInfo? oldValue, CardInfo? newValue)
+	{
+		if (oldValue is not null)
+		{
+			oldValue.CardTemplate = "CardViewCompressed";
+		}
+		if (newValue is not null)
+		{
+			newValue.CardTemplate = "CardViewDefault";
+		}
+	}
 
 	public MainPage()
 	{
 		BindingContext = this;
 		InitializeComponent();
-		CounterBtn.SetBinding(Button.TextProperty, static (MainPage ctx) => ctx.Count, BindingMode.OneWay, stringFormat: "Clicked {0} times");
-	}
-
-	void OnCounterClicked(object? sender, EventArgs e)
-	{
-		Count++;
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }

8f88783f2f73994efee393050443fcb4a91e0438
Stephen Quan <squan@esri.com>
2025-06-02 18:21:07 +1000

    Update GitVersion

--- src/MauiHelpers.Mvvm.SourceGenerators/AttributeNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiHelpers.Mvvm.SourceGenerators;
-
-static class AttributeNames
-{
-	public const string MvvmNamespace = "MauiHelpers.Mvvm";
-
-	public const string ObservableProperty = $"{MvvmNamespace}.ObservablePropertyAttribute";
-	public const string BindableProperty = $"{MvvmNamespace}.BindablePropertyAttribute";
-}

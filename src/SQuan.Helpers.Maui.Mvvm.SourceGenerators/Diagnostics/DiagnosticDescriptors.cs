c4f0dc624bd2c5ef79ee7dd1ccbfb7b0458fb577
Stephen Quan <squan@esri.com>
2025-06-07 16:06:25 +1000

    Revert "Update DiagnosticsDescriptors"
    This reverts commit 426d0228809b2e0944ab24e0be02cd3f430c0f8c.

--- src/SQuan.Helpers.Maui.Mvvm.SourceGenerators/Diagnostics/DiagnosticDescriptors.cs
+++ src/SQuan.Helpers.Maui.Mvvm.SourceGenerators/Diagnostics/DiagnosticDescriptors.cs
@@ -23,7 +23,7 @@ public static class DiagnosticDescriptors
 		id: "SQMVVMTK0002",
 		title: "C# language version is not 'preview'",
 		messageFormat: """Using [BindableProperty] on partial properties requires the C# language version to be set to 'preview', as support for the 'field' keyword is needed by the source generators to emit valid code (add <LangVersion>preview</LangVersion> to your .csproj/.props file)""",
-		category: typeof(BindablePropertyGenerator).FullName,
+		category: typeof(ObservablePropertyGenerator).FullName,
 		defaultSeverity: DiagnosticSeverity.Error,
 		isEnabledByDefault: true,
 		description: "The C# language version must be set to 'preview' when using [BindableProperty] on partial properties for the source generators to emit valid code (the <LangVersion>preview</LangVersion> option must be set in the .csproj/.props file).",
